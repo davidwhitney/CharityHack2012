7540be434239606e6613f1f92a47a18e079e36c0
David Whitney <david@davidwhitney.co.uk>
2012-11-03 22:33:36 +0000

    REFUCKTOR!

--- CharityHack2012/Code/Adapters/CharityComissionAdapter.cs
+++ CharityHack2012/Code/Adapters/CharityComissionAdapter.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
-using System.Web;
 using CharityHack2012.Code.Http;
 using CharityHack2012.Models;
 using HtmlAgilityPack;
