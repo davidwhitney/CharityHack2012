763770478e3ea2a6a16ea95888f6409ee4bce6c1
David Whitney <david@davidwhitney.co.uk>
2012-11-03 16:00:10 +0000

    Added first test!

--- CharityHack2012.Test.Unit/Code/Adapters/CharityComissionAdapterTests.cs
+++ CharityHack2012.Test.Unit/Code/Adapters/CharityComissionAdapterTests.cs
@@ -1,6 +1,27 @@
-﻿namespace CharityHack2012.Test.Unit.Code.Adapters
+﻿using CharityHack2012.Code.Adapters;
+using NUnit.Framework;
+
+namespace CharityHack2012.Test.Unit.Code.Adapters
 {
+    [TestFixture]
     public class CharityComissionAdapterTests
     {
+        private CharityComissionAdapter _adapter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _adapter = new CharityComissionAdapter();
+        }
+
+        [Test]
+        public void CharityComissionUriForRegistrationNumber_WhenProvidedWithARegNo_GeneratesAppropriateUrl()
+        {
+            const string regNo = "12345";
+
+            var uri = _adapter.CharityComissionUriForRegistrationNumber(regNo);
+
+            Assert.That(uri, Is.EqualTo(_adapter.CharityComissionBaseUri + _adapter.UrlPart + regNo));
+        }
     }
 }
--- CharityHack2012/Code/Adapters/CharityComissionAdapter.cs
+++ CharityHack2012/Code/Adapters/CharityComissionAdapter.cs
@@ -7,5 +7,12 @@ namespace CharityHack2012.Code.Adapters
 {
     public class CharityComissionAdapter
     {
+        public string CharityComissionBaseUri { get { return "http://www.charity-commission.gov.uk"; } }
+        public string UrlPart { get { return "/Showcharity/RegisterOfCharities/CharityWithPartB.aspx?SubsidiaryNumber=0&RegisteredCharityNumber=";; } }
+
+        public string CharityComissionUriForRegistrationNumber(string registrationNumber)
+        {
+            return CharityComissionBaseUri + UrlPart + registrationNumber;
+        }
     }
 }
