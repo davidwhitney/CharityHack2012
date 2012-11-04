74578197c5b4ed3a9e481f743c33324962218885
David Whitney <david@davidwhitney.co.uk>
2012-11-04 05:10:07 +0000

    refactored load charity profile.

--- CharityHack2012/Controllers/CharityController.cs
+++ CharityHack2012/Controllers/CharityController.cs
@@ -1,8 +1,10 @@
 using System.Web.Mvc;
 using CharityHack2012.Code.Adapters;
+using CharityHack2012.Models;
 using JustGiving.Api.Sdk;
 using System.Linq;
 using JustGiving.Api.Sdk.Model.Search;
+using CharitySearchResult = JustGiving.Api.Sdk.Model.Search.CharitySearchResult;
 
 namespace CharityHack2012.Controllers
 {
@@ -21,17 +23,8 @@ namespace CharityHack2012.Controllers
 
         public ActionResult Index(string id)
         {
-            var charityProfile = _charityComissionAdapter.LoadByRegNo(id);
-            var charityNewsOnGuardian = _guardianApiAdapter.SearchContentByCharityName(charityProfile.CharityName);
-            charityProfile.NewsItems = charityNewsOnGuardian.Response.Results; 
-
-            var vaguelyMatchingCharities = _jgClient.Search.CharitySearch(id);
-            var thisCharity =
-                vaguelyMatchingCharities.Results.FirstOrDefault(
-                    x => x.RegistrationNumber.Contains(id) && charityProfile.CharityName.Contains(x.Name.ToLower()));
-
-            charityProfile.JgCharityData = thisCharity ?? new CharitySearchResult();
-            charityProfile.CharityImage = "http://v3-sandbox.justgiving.com" + (thisCharity == null ? "" : thisCharity.LogoFileName);
+            CharitySearchResult thisCharity;
+            var charityProfile = LoadCharityProfile(id, out thisCharity);
 
             if (thisCharity != null)
             {
@@ -44,6 +37,26 @@ namespace CharityHack2012.Controllers
             }
 
             return View(charityProfile);
+        }
+
+        public ActionResult Compare(string charityId1, string charityId2)
+        {
+            return View();
+        }
+
+        private CharityProfile LoadCharityProfile(string id, out CharitySearchResult thisCharity)
+        {
+            var charityProfile = _charityComissionAdapter.LoadByRegNo(id);
+            var charityNewsOnGuardian = _guardianApiAdapter.SearchContentByCharityName(charityProfile.CharityName);
+            charityProfile.NewsItems = charityNewsOnGuardian.Response.Results;
+
+            var vaguelyMatchingCharities = _jgClient.Search.CharitySearch(id);
+            thisCharity = vaguelyMatchingCharities.Results.FirstOrDefault(
+                x => x.RegistrationNumber.Contains(id) && charityProfile.CharityName.Contains(x.Name.ToLower()));
+
+            charityProfile.JgCharityData = thisCharity ?? new CharitySearchResult();
+            charityProfile.CharityImage = "http://v3-sandbox.justgiving.com" + (thisCharity == null ? "" : thisCharity.LogoFileName);
+            return charityProfile;
         }
     }
 }
