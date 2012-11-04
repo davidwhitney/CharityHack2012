d9bb6d6a427bfa0860037a85fecd1c299c7d2177
David Whitney <david@davidwhitney.co.uk>
2012-11-04 00:50:06 +0000

    added search.

--- CharityHack2012/Controllers/HomeController.cs
+++ CharityHack2012/Controllers/HomeController.cs
@@ -3,14 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JustGiving.Api.Sdk;
 
 namespace CharityHack2012.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private readonly IJustGivingClient _jgClient;
+
+        public HomeController(IJustGivingClient jgClient)
         {
-            return View();
+            _jgClient = jgClient;
         }
+
+        [HttpGet]
+        public ActionResult Index(string query = null)
+        {
+            var resultsForRender = new List<CharitySearchResult>();
+
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                var results = _jgClient.Search.CharitySearch(query);
+
+                resultsForRender.AddRange(results.Results.Select(charitySearchResult => new CharitySearchResult
+                    {
+                        CharityDisplayName = charitySearchResult.Name,
+                        CharityRegistrationNumber = charitySearchResult.RegistrationNumber,
+                        JgCharityId = Int32.Parse(charitySearchResult.CharityId)
+                    }));
+            }
+
+            return View(resultsForRender);
+        }
+
+        [HttpPost]
+        public ActionResult Search(string query)
+        {
+            return RedirectToAction("Index", "Home", new {query = query});
+        }
+    }
+
+    public class CharitySearchResult
+    {
+        public int JgCharityId { get; set; }
+        public string CharityDisplayName { get; set; }
+        public string CharityRegistrationNumber { get; set; }
     }
 }
